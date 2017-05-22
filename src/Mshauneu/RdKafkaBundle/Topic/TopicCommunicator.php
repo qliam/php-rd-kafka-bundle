d7ea9d3372cf997d2ad5ed6e5801981da143d32c
Q Liam <qliam@users.noreply.github.com>
2017-05-22 18:54:33 +0200

    Update TopicCommunicator.php
    Hard fix to avoid timeout when produce

--- src/Mshauneu/RdKafkaBundle/Topic/TopicCommunicator.php
+++ src/Mshauneu/RdKafkaBundle/Topic/TopicCommunicator.php
@@ -48,6 +48,12 @@ abstract class TopicCommunicator {
 	 */
 	protected function getConfig($props) {
 		$conf = new Conf();
+		// We can change the amount of time a socket blocking operation lasts for, which will help librdkafka release your application to continue
+		$conf->set('socket.blocking.max.ms', 1);
+		// we can also set the buffering time, so we dispatch asap:
+		$conf->set('queue.buffering.max.ms', 1);
+		// Finally, we can say to only wait for X messages before sending to Kafka. In a lightweight / custom app, you may only be sending one message, so get it out there straight away
+		$conf->set('queue.buffering.max.messages', 10);
 		if (null !== $props) {
 			foreach ($props as $name => $value) {
 				$conf->set(str_replace("_", ".", $name), $value);
